92725f19557e145a53bcd565413a97957ab2c55e
PierreLouisBaudrand <43134780+PierreLouisBaudrand@users.noreply.github.com>
2022-05-04 22:22:48 +0200

    v1.0
    select / insert / delete

---
+++ Firebase/Firebase/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Firebase
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messagebox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.insertbtn = new System.Windows.Forms.Button();
+            this.selectbtn = new System.Windows.Forms.Button();
+            this.updatebtn = new System.Windows.Forms.Button();
+            this.deletebtn = new System.Windows.Forms.Button();
+            this.usernamebox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.themmessagebox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messagebox
+            // 
+            this.messagebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messagebox.Location = new System.Drawing.Point(225, 180);
+            this.messagebox.Name = "messagebox";
+            this.messagebox.Size = new System.Drawing.Size(251, 26);
+            this.messagebox.TabIndex = 0;
+            this.messagebox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "votre msg";
+            // 
+            // insertbtn
+            // 
+            this.insertbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insertbtn.Location = new System.Drawing.Point(136, 315);
+            this.insertbtn.Name = "insertbtn";
+            this.insertbtn.Size = new System.Drawing.Size(80, 29);
+            this.insertbtn.TabIndex = 8;
+            this.insertbtn.Text = "INSERT";
+            this.insertbtn.UseVisualStyleBackColor = true;
+            this.insertbtn.Click += new System.EventHandler(this.insertbtn_Click);
+            // 
+            // selectbtn
+            // 
+            this.selectbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selectbtn.Location = new System.Drawing.Point(234, 315);
+            this.selectbtn.Name = "selectbtn";
+            this.selectbtn.Size = new System.Drawing.Size(80, 29);
+            this.selectbtn.TabIndex = 9;
+            this.selectbtn.Text = "SELECT";
+            this.selectbtn.UseVisualStyleBackColor = true;
+            this.selectbtn.Click += new System.EventHandler(this.selectbtn_Click);
+            // 
+            // updatebtn
+            // 
+            this.updatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updatebtn.Location = new System.Drawing.Point(330, 315);
+            this.updatebtn.Name = "updatebtn";
+            this.updatebtn.Size = new System.Drawing.Size(88, 29);
+            this.updatebtn.TabIndex = 10;
+            this.updatebtn.Text = "UPDATE";
+            this.updatebtn.UseVisualStyleBackColor = true;
+            // 
+            // deletebtn
+            // 
+            this.deletebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.deletebtn.Location = new System.Drawing.Point(439, 315);
+            this.deletebtn.Name = "deletebtn";
+            this.deletebtn.Size = new System.Drawing.Size(88, 29);
+            this.deletebtn.TabIndex = 11;
+            this.deletebtn.Text = "DELETE";
+            this.deletebtn.UseVisualStyleBackColor = true;
+            this.deletebtn.Click += new System.EventHandler(this.deletebtn_Click);
+            // 
+            // usernamebox
+            // 
+            this.usernamebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernamebox.Location = new System.Drawing.Point(225, 223);
+            this.usernamebox.Name = "usernamebox";
+            this.usernamebox.Size = new System.Drawing.Size(251, 26);
+            this.usernamebox.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(139, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "username";
+            // 
+            // themmessagebox
+            // 
+            this.themmessagebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.themmessagebox.Location = new System.Drawing.Point(225, 266);
+            this.themmessagebox.Name = "themmessagebox";
+            this.themmessagebox.Size = new System.Drawing.Size(251, 26);
+            this.themmessagebox.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(141, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "leur msg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.themmessagebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.usernamebox);
+            this.Controls.Add(this.deletebtn);
+            this.Controls.Add(this.updatebtn);
+            this.Controls.Add(this.selectbtn);
+            this.Controls.Add(this.insertbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messagebox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox messagebox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button insertbtn;
+        private System.Windows.Forms.Button selectbtn;
+        private System.Windows.Forms.Button updatebtn;
+        private System.Windows.Forms.Button deletebtn;
+        private System.Windows.Forms.TextBox usernamebox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox themmessagebox;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Firebase/Firebase/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Windows.Forms;
+using FireSharp.Config;
+using FireSharp.Response;
+using FireSharp.Interfaces;
+
+namespace Firebase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        IFirebaseConfig fcon = new FirebaseConfig()
+        {
+            AuthSecret = "lVE5xwYYR6693QYet0L3XAteKwAObA2Y4hS6W6lv",
+            BasePath = "https://chatapplimayrac-default-rtdb.firebaseio.com/"
+        };
+
+        IFirebaseClient fclient;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try 
+            {
+                fclient = new FireSharp.FirebaseClient(fcon);
+            }
+            catch
+            {
+                MessageBox.Show("il y a eu un problème avec internet");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insertbtn_Click(object sender, EventArgs e)
+        {
+            Message msg = new Message()
+            {
+                MessageTxt = messagebox.Text,
+                UserName = usernamebox.Text
+            };
+
+            var setter = fclient.Set("Messages/channel1/" + usernamebox.Text, msg);
+        }
+
+        private void selectbtn_Click(object sender, EventArgs e)
+        {
+            var result = fclient.Get("Messages/channel1/" + usernamebox.Text);
+            Message msg = result.ResultAs<Message>();
+            themmessagebox.Text = msg.MessageTxt;
+        }
+
+        private void deletebtn_Click(object sender, EventArgs e)
+        {
+            var setter = fclient.Delete("Messages/channel1/" + usernamebox.Text);
+        }
+    }
+}
